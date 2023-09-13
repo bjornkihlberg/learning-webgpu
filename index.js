d4bdb04d0db04faff6dddb36c63ff57fd09ef921
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2023-09-13 21:41:59 +0200

    mesh buffer data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** Initialize WebGPU
 @param {HTMLCanvasElement} canvas
 @returns {Promise<[GPUDevice, GPUCanvasContext]>}
- */
+*/
 export async function initialization(canvas) {
   if (!navigator.gpu) throw new Error("WebGPU is not supported on your browser or device.")
   const adapter = await navigator.gpu.requestAdapter()
@@ -12,4 +12,23 @@ export async function initialization(canvas) {
   const format = navigator.gpu.getPreferredCanvasFormat();
   context.configure({ device, format });
   return [device, context]
+}
+
+/**
+@param {GPUDevice} device
+@param {number} vertexByteLength
+@param {number} indexByteLength
+@returns {{ vertexBuffer: GPUBuffer, indexBuffer: GPUBuffer }}
+*/
+export function createMeshBuffers(device, vertexByteLength, indexByteLength) {
+  return {
+    vertexBuffer: device.createBuffer({
+      size: vertexByteLength,
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+    }),
+    indexBuffer: device.createBuffer({
+      size: indexByteLength,
+      usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST
+    }),
+  }
 }
