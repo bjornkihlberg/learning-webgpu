c3910db18a8525d841f14c6a76214ddfa3ae0308
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2023-09-15 15:51:09 +0200

    set up camera uniform buffer

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+export const sizeOfFloat32 = 4
+
 /** Initialize WebGPU
 @param {HTMLCanvasElement} canvas
 @returns {Promise<[GPUDevice, GPUCanvasContext, GPUTextureFormat]>}
@@ -31,4 +33,90 @@ export function createMeshBuffers(device, vertexByteLength, indexByteLength) {
       usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST
     }),
   }
+}
+
+/**
+@param {GPUDevice} device
+@param {GPUTextureFormat} format
+@returns {GPURenderPipeline}
+*/
+export function createMainPipeline(device, format) {
+  const mainShaderCode = `
+    @group(0)
+    @binding(0)
+    var<uniform> camera: mat4x4f;
+
+    struct VsInput {
+      @location(0) position: vec2f,
+    };
+
+    struct VsOutput {
+      @builtin(position) position: vec4f,
+    };
+
+    @vertex
+    fn vertexShader(vsIn: VsInput) -> VsOutput {
+      var vsOut: VsOutput;
+      vsOut.position = camera * vec4f(vsIn.position * 0.5, 0, 1);
+      return vsOut;
+    }
+
+    @fragment
+    fn fragmentShader(vsOut: VsOutput) -> @location(0) vec4f {
+      return vec4f(1, 0, 0, 1);
+    }`
+
+  const mainShaderModule = device.createShaderModule({
+    label: "main-shader",
+    code: mainShaderCode,
+  })
+
+  /** @type {GPUBindGroupLayoutEntry} */
+  const cameraUniformBindGroupLayout = {
+    binding: 0,
+    visibility: GPUShaderStage.VERTEX,
+    buffer: {
+      type: "uniform",
+      minBindingSize: sizeOfFloat32 * 4 * 4
+    }
+  }
+
+  /** @type {GPUVertexAttribute} */
+  const positionAttribute = {
+    format: "float32x2",
+    offset: 0,
+    shaderLocation: 0,
+  }
+
+  /** @type {GPUVertexBufferLayout} */
+  const vertexLayout = {
+    arrayStride: sizeOfFloat32 * 2,
+    attributes: [positionAttribute],
+  }
+
+  return device.createRenderPipeline({
+    label: "main-pipeline",
+    layout: device.createPipelineLayout({
+      label: "main-pipeline-layout",
+      bindGroupLayouts: [device.createBindGroupLayout({
+        label: "main-bind-group-layout",
+        entries: [cameraUniformBindGroupLayout]
+      })]
+    }),
+    vertex: {
+      module: mainShaderModule,
+      entryPoint: "vertexShader",
+      buffers: [vertexLayout],
+    },
+    fragment: {
+      module: mainShaderModule,
+      entryPoint: "fragmentShader",
+      targets: [{ format }],
+    },
+    primitive: {
+      cullMode: "back",
+      frontFace: "ccw",
+      topology: "triangle-list",
+    },
+  })
 }
